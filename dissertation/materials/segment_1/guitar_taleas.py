c67d9fbff5f78110b698564af519901fbb653dfe
Joseph Davancens <josephdavancens@gmail.com>
2016-04-20 12:45:28 -0700

    Update guitar_taleas.py

--- dissertation/materials/segment_1/guitar_taleas.py
+++ dissertation/materials/segment_1/guitar_taleas.py
@@ -4,19 +4,19 @@ from abjad import *
 
 taleas = {
     '2': rhythmmakertools.Talea(
-        talea=[],
+        talea=[6, 6, 7, 6, 5, 5, 9, 10, 5, 6],
         denominator=16
     ),
     '3': rhythmmakertools.Talea(
-        talea=[],
+        talea=[4, 5, 7, 7, 9, 5, 6, 10, 5, 6],
         denominator=16
     ),
     '4': rhythmmakertools.Talea(
-        talea=[],
+        talea=[3, 4, 4, 3, 7, 5, 4, 3, 10, 5],
         denominator=16
     ),
     '5': rhythmmakertools.Talea(
-        talea=[],
+        talea=[2, 3, 3, 4, 4, 2, 2, 4, 5, 4],
         denominator=16
     ),
 }
