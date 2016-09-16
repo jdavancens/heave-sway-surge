ef310288a374becedc87ee1faf03694c2f0c98ad
jdavancens <josephdavancens@gmail.com>
2016-09-16 12:09:01 -0700

    Update __init__.py for removed packages

--- surge/__init__.py
+++ surge/__init__.py
@@ -5,8 +5,7 @@ from surge.tools.actions import *
 from surge.tools.bezzie import *
 from surge.tools.generators import *
 from surge.tools.handlers import *
+from surge.tools.illustrationtools import *
 from surge.tools.makers import *
-from surge.tools.noisetools import *
-from surge.tools.ratiotools import *
 from surge.tools.rhythmtools import *
 from surge.tools.templates import *
