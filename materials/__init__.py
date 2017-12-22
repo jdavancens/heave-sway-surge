4b37c5758d546064c3d49618823fedfcaa125fd8
Joseph Davancens <josephdavancens@gmail.com>
2017-12-21 21:31:28 -0800

    Remove materials and segments directories from root

--- materials/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import materials.observer
-import materials.woodwindfingerings
-
-from .instrument_categories import instrument_categories
-from .instruments import instruments
-from .staff_map import staff_map
--- materials/woodwindfingerings/clarinet/illustrate_basic_fingering_map.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from abjad.tools.instrumenttools.ClarinetInBFlat import ClarinetInBFlat
-from materials.woodwindfingerings.clarinet.basic_fingering_map import \
-    basic_fingering_map
-from surge.illustration.illustrate_fingerings import illustrate_fingerings
-
-clarinet = ClarinetInBFlat()
-illustrate_fingerings(clarinet, basic_fingering_map)
