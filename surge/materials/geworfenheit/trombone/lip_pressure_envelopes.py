cca4e673868e3dd7c241589f3dab54c897df58b6
Joseph Davancens <josephdavancens@gmail.com>
2018-01-18 07:49:37 -0800

    Adjust stages 5,6 trombone materials. Closes #19

--- surge/materials/geworfenheit/trombone/lip_pressure_envelopes.py
+++ surge/materials/geworfenheit/trombone/lip_pressure_envelopes.py
@@ -2,13 +2,18 @@
 from surge.materials.geworfenheit.durations import stage_durations
 from surge.tools.bezzie.BezierCurve import BezierCurve
 from surge.tools.bezzie.Path import Path
+from surge.tools.bezzie.RandomInterpolater import RandomInterpolater
+
+random_interpolater = RandomInterpolater(seed=hash('enobmort'))
 
 lip_pressure_envelopes = (
     None,
     None,
     Path(BezierCurve((0, 0.5), (stage_durations[2], 0.5))),
     Path(BezierCurve((0, 0.5), (stage_durations[3], 0.5))),
-    Path(BezierCurve((0, 0.5), (stage_durations[4], 0.5))),
-    Path(BezierCurve((0, 0.5), (stage_durations[5], 0.5))),
+    Path(BezierCurve((0, 0), (stage_durations[4], 1))),
+    Path(BezierCurve((0, 0), (stage_durations[5], 1))),
     None
 )
+lip_pressure_envelopes[4].set_interpolater(random_interpolater)
+lip_pressure_envelopes[5].set_interpolater(random_interpolater)
--- surge/materials/geworfenheit/trombone/rhythm_makers_slide.py
+++ surge/materials/geworfenheit/trombone/rhythm_makers_slide.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from .rest_indices import rest_indices
 from surge.materials.geworfenheit.rest_indices import rest_indices_by_group
+from surge.materials.geworfenheit.time_signatures import time_signatures
+from surge.tools.makers.RatioMaker import RatioMaker
+from surge.tools.rhythmtools.ConstantProlater import ConstantProlater
+from surge.tools.rhythmtools.UnitSubdivider import UnitSubdivider
 import abjad
 
 rhythm_makers_slide = [None, None]
@@ -35,35 +39,47 @@ rhythm_maker_stage_4 = abjad.rhythmmakertools.NoteRhythmMaker(
 )
 rhythm_makers_slide.append(rhythm_maker_stage_4)
 
-# stage 5
+# Stage 5
+# 9:8
+# 012345678
+# -..*.*--*
 
-rhythm_maker_stage_5 = abjad.rhythmmakertools.NoteRhythmMaker(
-    division_masks=[
-        abjad.silence_every(
-            indices=rest_indices_by_group['d'][4],
-            period=35
-        )
-    ],
-    tie_specifier=abjad.rhythmmakertools.TieSpecifier(
-        tie_across_divisions=True
+ratio_maker_stage_5 = RatioMaker(
+    time_signatures=time_signatures[4],
+    rest_indices=rest_indices_by_group["d"][4],
+    prolater=ConstantProlater([9]),
+    subdivider=UnitSubdivider(
+        multiplier=1,
+        rotation_cycle=range(12),
+        sustain_mask=abjad.sustain_every(indices=[0, 6, 7], period=9),
+        silence_mask=abjad.silence_every(indices=[3, 5, 8], period=9)
     )
+)
+rhythm_maker_stage_5 = abjad.rhythmmakertools.TupletRhythmMaker(
+    tuplet_ratios=ratio_maker_stage_5()
 )
 rhythm_makers_slide.append(rhythm_maker_stage_5)
 
-# stage 6
+# Stage 6
+# 9:8
+# 012345678
+# -..*.*--*
 
-rhythm_maker_stage_6 = abjad.rhythmmakertools.NoteRhythmMaker(
-    division_masks=[
-        abjad.silence_every(
-            indices=rest_indices_by_group['h'][5],
-            period=35
-        )
-    ],
-    tie_specifier=abjad.rhythmmakertools.TieSpecifier(
-        tie_across_divisions=True
+ratio_maker_stage_6 = RatioMaker(
+    time_signatures=time_signatures[5],
+    rest_indices=rest_indices_by_group["h"][5],
+    prolater=ConstantProlater([9]),
+    subdivider=UnitSubdivider(
+        multiplier=1,
+        rotation_cycle=range(9),
+        sustain_mask=abjad.sustain_every(indices=[0, 6, 7], period=9),
+        silence_mask=abjad.silence_every(indices=[3, 5, 8, 9, 11], period=9)
     )
 )
-rhythm_makers_slide.append(rhythm_maker_stage_5)
+rhythm_maker_stage_6 = abjad.rhythmmakertools.TupletRhythmMaker(
+    tuplet_ratios=ratio_maker_stage_6()
+)
+rhythm_makers_slide.append(rhythm_maker_stage_6)
 
 # stage 7
 
